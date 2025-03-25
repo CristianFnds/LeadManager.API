0880c974ac9f08cb53ffb2deb68069ef3fba3431
Cristian <cristianfernandes2013@gmail.comm>
2025-03-24 21:34:05 -0300

    ADicionando projeto

---
+++ LeadManager.API/Program.cs
@@ -0,0 +1,49 @@
+using LeadManager.Application.Interfaces;
+using LeadManager.Application.Service;
+using LeadManager.Domain.Interfaces;
+using LeadManager.Infrastructure.Databases;
+using LeadManager.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LeadManagerDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<ILeadRepository, LeadRepository>();
+builder.Services.AddScoped<ILeadService, LeadService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") 
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowFrontend");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ LeadManager.Application/Service/LeadService.cs
@@ -0,0 +1,41 @@
+﻿using LeadManager.Application.Interfaces;
+using LeadManager.Domain.Entities;
+using LeadManager.Domain.Interfaces;
+
+namespace LeadManager.Application.Service
+{
+    public class LeadService : ILeadService
+    {
+        private readonly ILeadRepository _invitedRepository;
+
+        public LeadService(ILeadRepository invitedRepository)
+        {
+            _invitedRepository = invitedRepository;
+        }
+
+        public async Task<IEnumerable<Lead>> GetAllAsync()
+        {
+            return await _invitedRepository.GetAllAsync();
+        }
+
+        public async Task<Lead> GetByIdAsync(int id)
+        {
+            return await _invitedRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Lead invited)
+        {
+            await _invitedRepository.AddAsync(invited);
+        }
+
+        public async Task UpdateAsync(Lead invited)
+        {
+            await _invitedRepository.UpdateAsync(invited);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _invitedRepository.DeleteAsync(id);
+        }
+    }
+}
